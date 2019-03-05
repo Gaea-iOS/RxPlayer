c74dbdcd6e73c88085cf0970d9bf9bc478c4ea26
jerry <445242970@qq.com>
2019-03-05 12:05:54 +0800

    private some methods

--- RxPlayer/Classes/ObservableType+Extension.swift
+++ RxPlayer/Classes/ObservableType+Extension.swift
@@ -9,22 +9,22 @@ import RxSwift
 
 extension ObservableType {
 
-    public func filterNil<T>() -> Observable<T?> where E == T? {
+    func filterNil<T>() -> Observable<T?> where E == T? {
         return filter { $0 == nil }
     }
 
-    public func mapToVoid() -> Observable<()> {
+    func mapToVoid() -> Observable<()> {
         return mapTo(())
     }
 }
 
 extension ObservableType where E == Bool {
 
-    public func filterTrue() -> Observable<Bool> {
+    func filterTrue() -> Observable<Bool> {
         return filter { $0 }
     }
 
-    public func filterFalse() -> Observable<Bool> {
+    func filterFalse() -> Observable<Bool> {
         return filter { !$0 }
     }
 }
